19e7f72df617cf08995e51d16e04cd10f7414aa3
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-02-17 13:10:03 -0500

    register and save user to db

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -20,6 +23,30 @@ function SignUp() {
       [e.target.id]: e.target.value
     }))
   }
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
@@ -29,8 +56,8 @@ function SignUp() {
             welcome back
           </p>
         </header>
-        <form>
-        <input
+        <form onSubmit={onSubmit}>
+          <input
             type="text"
             className='nameInput'
             placeholder='Name'
